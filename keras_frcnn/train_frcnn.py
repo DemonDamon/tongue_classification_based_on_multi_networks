3b9d630e7546b7d28cd84136cd11c767d36219f4
Damon Li <bingzhenli@hotmail.com>
2019-03-07 15:03:49 +0800

    Commit Message

--- keras_frcnn/train_frcnn.py
+++ keras_frcnn/train_frcnn.py
@@ -55,8 +55,8 @@ C = config.Config()
 
 C.num_epochs = int(options.num_epochs)
 C.num_rois = int(options.num_rois)
-C.use_horizontal_flips = options.use_horizontal_flips == 'True'
-C.use_vertical_flips = options.use_vertical_flips == 'True'
+C.use_horizontal_flips = options.horizontal_flips == 'True'
+C.use_vertical_flips = options.vertical_flips == 'True'
 C.rot_90 = options.rot_90 == 'True'
 C.utilize_transfer_learning = options.utilize_transfer_learning == 'True'
 C.including_top_weight = options.including_top_weight == 'True'
