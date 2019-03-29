12e6bc78efd76330d3af80ea4868c7b315542041
liziran <bingzhenli@hotmail.com>
2019-03-29 09:27:41 +0800

    Commit Message

--- keras_rfcn/KerasRFCN/Model/BaseModel.py
+++ keras_rfcn/KerasRFCN/Model/BaseModel.py
@@ -221,13 +221,13 @@ class BaseModel(object):
         train_dataset, val_dataset: Training and validation Dataset objects.
         learning_rate: The learning rate to train with
         epochs: Number of training epochs. Note that previous training epochs
-                are considered to be done alreay, so this actually determines
-                the epochs to train in total rather than in this particaular
+                are considered to be done already, so this actually determines
+                the epochs to train in total rather than in this particular
                 call.
-        layers: Allows selecting wich layers to train. It can be:
+        layers: Allows selecting which layers to train. It can be:
             - A regular expression to match layer names to train
             - One of these predefined values:
-              heaads: The RPN, classifier and mask heads of the network
+              heads: The RPN, classifier and mask heads of the network
               all: All the layers
               3+: Train Resnet stage 3 and up
               4+: Train Resnet stage 4 and up
