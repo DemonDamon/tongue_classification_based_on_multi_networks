24dd647376b0595d9f8b0be710342c705db7c197
liziran <bingzhenli@hotmail.com>
2019-04-02 15:15:50 +0800

    Commit Message

--- keras_retinanet/keras_retinanet/bin/train.py
+++ keras_retinanet/keras_retinanet/bin/train.py
@@ -418,13 +418,17 @@ def parse_args(args):
 
 
 def main(args=None):
+    print(" " + "-" * 30 + " Preparing Configuration and Data " + "-" * 30)
+
     # parse arguments
     if args is None:
         args = sys.argv[1:]
     args = parse_args(args)
+    print(' [*] parsed arguments...')
 
     # create object that stores backbone information
     backbone = models.backbone(args.backbone)
+    print(' [*] created object that stores backbone information...')
 
     # make sure keras is the minimum required version
     check_keras_version()
@@ -437,26 +441,27 @@ def main(args=None):
     # optionally load config parameters
     if args.config:
         args.config = read_config_file(args.config)
+        print(' [*] optionally loaded config parameters...')
 
     # create the generators
     train_generator, validation_generator = create_generators(args, backbone.preprocess_image)
+    print(' [*] created the data generator of both train and validation...')
 
     # create the model
     if args.snapshot is not None:
-        print('Loading model, this may take a second...')
         model            = models.load_model(args.snapshot, backbone_name=args.backbone)
         training_model   = model
         anchor_params    = None
         if args.config and 'anchor_parameters' in args.config:
             anchor_params = parse_anchor_parameters(args.config)
         prediction_model = retinanet_bbox(model=model, anchor_params=anchor_params)
+        print(' [*] loaded model successfully...')
     else:
         weights = args.weights
         # default to imagenet if nothing else is specified
         if weights is None and args.imagenet_weights:
             weights = backbone.download_imagenet()
 
-        print('Creating model, this may take a second...')
         model, training_model, prediction_model = create_models(
             backbone_retinanet=backbone.retinanet,
             num_classes=train_generator.num_classes(),
@@ -466,8 +471,10 @@ def main(args=None):
             lr=args.lr,
             config=args.config
         )
+        print(' [*] created model, this may take a second...')
 
     # print model summary
+    print(' [*] print model summary: ')
     print(model.summary())
 
     # this lets the generator compute backbone layer shapes using the actual backbone model
@@ -482,8 +489,10 @@ def main(args=None):
         training_model,
         prediction_model,
         validation_generator,
+        validation_generator,
         args,
     )
+    print(' [*] created the callbacks...')
 
     # Use multiprocessing if workers > 0
     if args.workers > 0:
@@ -492,6 +501,8 @@ def main(args=None):
         use_multiprocessing = False
 
     # start training
+    print(" " + "-" * 30 + " Starting training " + "-" * 30)
+
     training_model.fit_generator(
         generator=train_generator,
         steps_per_epoch=args.steps,
